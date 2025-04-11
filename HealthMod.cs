b9a0ef5fed9a158bdf68d5c87c73c0f0bbe6d107
Voidane <hero4aquest@gmail.com>
2025-04-11 05:21:59 -0400

    Default health bar

---
+++ HealthMod.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Il2CppScheduleOne.PlayerScripts.Health;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Health
+{
+    public class HealthMod : MelonMod
+    {
+        public static Transform UI;
+        public static Transform HUD;
+        public static bool mainSceneLoaded;
+
+        public static GameObject healthHolder;
+        public HealthBarController healthController;
+        public static HealthUICreator healthUI;
+
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg("Initializing mod!");
+            HarmonyPatches();
+        }
+
+        private void HarmonyPatches()
+        {
+            HarmonyLib.Harmony harmony = new HarmonyLib.Harmony("com.voidane.healthmod");
+
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == "Main")
+            {
+                mainSceneLoaded = true;
+                MelonCoroutines.Start(WaitOnSceneLoad(null, "UI", 20.0F, (_UI) =>
+                {
+                    UI = _UI;
+                    MelonCoroutines.Start(WaitOnSceneLoad(UI, "HUD", 5.0F, (_HUD) =>
+                    {
+                        HUD = _HUD;
+                        if (HealthUICreator.Instance == null)
+                        {
+                            MelonLogger.Msg("Creating new Health bar");
+                            new HealthUICreator();
+                        }
+                    }));
+                }));
+            }
+        }
+
+        private IEnumerator WaitOnSceneLoad(Transform parent, string name, float timeoutLimit, Action<Transform> onComplete)
+        {
+            Transform target = null;
+            float timeOutCounter = 0F;
+            int attempt = 0;
+
+            while (target == null && timeOutCounter < timeoutLimit)
+            {
+                target = (parent == null) ? GameObject.Find(name).transform : parent.Find(name);
+                if (target == null)
+                {
+                    timeOutCounter += 0.5F;
+                    yield return new WaitForSeconds(0.5F);
+                }
+            }
+
+            if (target != null)
+            {
+                onComplete?.Invoke(target);
+            }
+            else
+            {
+                MelonLogger.Error("Failed to find target object within timeout period!");
+                onComplete?.Invoke(null);
+            }
+
+            yield return target;
+        }
+    }
+}
---
+++ HealthUICreator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using MelonLoader;
+
+namespace Health
+{
+    public class HealthUICreator
+    {
+        public static HealthUICreator Instance;
+
+        public static GameObject healthHolder;
+        public static GameObject border;
+        public static GameObject fill;
+
+        public HealthUICreator()
+        {
+            Instance = this;
+
+            healthHolder = new GameObject("Health Manager");
+            healthHolder.transform.SetParent(HealthMod.HUD, false);
+
+            RectTransform healthHolder_RectTransform = healthHolder.AddComponent<RectTransform>();
+            healthHolder_RectTransform.anchorMin = new Vector2(0.35F, 0.11F); // Y Higher value lifts bottom
+            healthHolder_RectTransform.anchorMax = new Vector2(0.65F, 0.05F); // Y Lower value shortens bar vert
+            healthHolder_RectTransform.pivot = new Vector2(0.5F, 0);
+
+            border = new GameObject("Border");
+            border.transform.SetParent(healthHolder.transform, false);
+
+            RectTransform border_RectTransform = border.AddComponent<RectTransform>();
+            border_RectTransform.anchorMin = new Vector2(0, 0);
+            border_RectTransform.anchorMax = new Vector2(1, 1);
+            border_RectTransform.offsetMin = Vector2.zero;
+            border_RectTransform.offsetMax = Vector2.zero;
+
+            Image border_Image = border.AddComponent<Image>();
+            border_Image.color = new Color(34F/255F, 34F/255F, 34F/255F);
+
+            fill = new GameObject("Fill");
+            fill.transform.SetParent(healthHolder.transform, false);
+
+            RectTransform fill_RectTransform = fill.AddComponent<RectTransform>();
+            fill_RectTransform.anchorMin = new Vector2(0F, 0F);
+            fill_RectTransform.anchorMax = new Vector2(1F, 1F);
+            fill_RectTransform.offsetMin = new Vector2(2, 2);
+            fill_RectTransform.offsetMax = new Vector2(-2, -2);
+
+            Image fill_Image = fill.AddComponent<Image>();
+            fill_Image.color = new Color(56f / 255f, 132f / 255f, 8f / 255f);
+
+            MelonLogger.Msg("Finished creating UI");
+        }
+    }
+}
